f704706adf1785c98326197b2d4aef54bb0a28ec
unknown <ffrov0000a@gmail.com>
2025-11-14 00:53:26 +0700

    first commit

---
+++ express.js
@@ -0,0 +1,26 @@
+const twvoucher = require('@fortune-inc/tw-voucher');
+import redeemVoucher from "./index.js";
+import twvoucher from "@ragez/tw-voucher";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Subscribe to Arpan Neupane's channel");
+});
+
+app.post("/api/v1/topup",async (req,res)=>{
+    //ใส่ req.body ตรงได้ไม่ต้องกลัว object exploit
+    twvoucher('เบอร์โทรศัพท์',req.body).then(redeemed => {
+        return res.json({
+            success: true,
+            amount: redeemed.amount
+        })
+    }).catch(() => {
+        return res.json({
+            msg: "ลิ้งอั่งเปาไม่ถูกต้อง"
+        })
+    })
+})
+
+export default app; 
